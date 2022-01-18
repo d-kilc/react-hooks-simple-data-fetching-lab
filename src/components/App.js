75a39c28fad080dd210821752b3d3bc58d8d057e
Daniel <dkilcullen25@gmail.com>
2022-01-18 16:26:02 -0600

    commit tree 64a291456339a272bae6454ffe3d7fc1f032f816

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
+import { useEffect, useState } from "react";
+
 // create your App component here
+export default function App() {
+    const [image, setImage] = useState('')
+
+    const API_URL = 'https://dog.ceo/api/breeds/image/random'
+    
+    useEffect(() => {
+        fetch(API_URL)
+        .then(res => res.json())
+        .then(data => {
+            setImage(data.message)
+        })
+        // console.log('side effect')
+    }, [])
+
+    // console.log('rendered')
+
+    return(
+        <>
+            { !image ? <p>Loading....</p> : <img alt='A Random Dog' src={image} />}
+        </>
+    )
+}
